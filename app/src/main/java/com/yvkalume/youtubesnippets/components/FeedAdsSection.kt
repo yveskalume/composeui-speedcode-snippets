00f498ff8013bb4b44afb220e4c1053405c498c8
yveskalume <yvkalume@gmail.com>
2023-10-02 14:43:15 +0200

    [component] : FeedAdsSection

---
+++ app/src/main/java/com/yvkalume/youtubesnippets/components/FeedAdsSection.kt
@@ -0,0 +1,82 @@
+package com.yvkalume.youtubesnippets.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yvkalume.youtubesnippets.R
+
+@Composable
+fun FeedAdsSection() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(start = 10.dp, end = 2.dp, top = 16.dp, bottom = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.wrapContentSize()
+        ) {
+            Text(
+                text = "Nurture your audience",
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = "Learn how to build your \nbrand and generate demand \non LinkedIn.",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.height(1.dp))
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Learn more")
+            }
+        }
+
+        Image(
+            painter = painterResource(id = R.drawable.working),
+            contentDescription = null,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop,
+        )
+        Icon(
+            imageVector = Icons.Default.MoreVert,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurfaceVariant,
+            modifier = Modifier.align(Alignment.Top)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun FeedAdsSectionPreview() {
+    MaterialTheme {
+        FeedAdsSection()
+    }
+}
